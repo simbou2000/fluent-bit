e65a42506bebe461df2a272519a2ac4210a639e2
Simon Bouchard <sbouchard@rbbn.com>
2026-09-15 15:03:26 -0400

    out_syslog: Add nested keys support
    Signed-off-by: Simon Bouchard <sbouchard@rbbn.com>

--- tests/runtime/out_syslog.c
+++ tests/runtime/out_syslog.c
@@ -1691,6 +1691,65 @@ void flb_test_dtls_mode_enables_tls()
 }
 #endif
 
+void flb_test_nested_keys_rfc5424()
+{
+    struct test_ctx *ctx;
+    int ret;
+    int num;
+
+    char *buf = "[1, {\"log.appname\":\"fluent-bit\", \"p_key\":\"1234\", \"nested_values\": {\"msg\":\"hello world\", \"hostname.key\":\"localhost\", \"sd_key\": {\"logtype\": \"access\",\"clustername\": \"mycluster\",\"namespace\": \"mynamespace\"}}}]";
+    size_t size = strlen(buf);
+
+    char *expected_strs[] = {"hello world", "1970-01-01T00:00:01.000000Z",
+                             "<14>1 1970-01-01T00:00:01.000000Z localhost fluent-bit 1234 - [nested_values.sd_key logtype=\"access\" clustername=\"mycluster\" namespace=\"mynamespace\"] ﻿hello world"};
+    struct str_list expected = {
+                                .size = sizeof(expected_strs)/sizeof(char*),
+                                .lists = &expected_strs[0],
+    };
+
+    clear_output_num();
+
+    ctx = test_ctx_create();
+    if (!TEST_CHECK(ctx != NULL)) {
+        TEST_MSG("test_ctx_create failed");
+        exit(EXIT_FAILURE);
+    }
+
+    ret = flb_output_set(ctx->flb, ctx->o_ffd,
+                         "match", "*",
+                         "syslog_format", "rfc5424",
+                         "syslog_message_key", "$nested_values['msg']",
+                         "syslog_sd_key", "$nested_values['sd_key']",
+                         "syslog_appname_key", "log.appname",
+                         "syslog_procid_key", "$p_key",
+                         "syslog_hostname_key", "$nested_values['hostname.key']",
+                         NULL);
+    TEST_CHECK(ret == 0);
+
+    ret = flb_output_set_test(ctx->flb, ctx->o_ffd,
+                         "formatter", cb_check_str_list,
+                          &expected, NULL);
+    TEST_CHECK(ret == 0);
+
+    /* Start the engine */
+    ret = flb_start(ctx->flb);
+    TEST_CHECK(ret == 0);
+
+    /* Ingest data sample */
+    ret = flb_lib_push(ctx->flb, ctx->i_ffd, (char *) buf, size);
+    TEST_CHECK(ret >= 0);
+
+    /* waiting to flush */
+    flb_time_msleep(500);
+
+    num = get_output_num();
+    if (!TEST_CHECK(num > 0))  {
+        TEST_MSG("no outputs");
+    }
+
+    test_ctx_destroy(ctx);
+}
+
 TEST_LIST = {
     /* rfc3164 */
     /* procid_key, msgid_key, sd_key are not supported */
@@ -1727,5 +1786,6 @@ TEST_LIST = {
     {"tls_mode_enables_tls", flb_test_tls_mode_enables_tls},
     {"dtls_mode_enables_tls", flb_test_dtls_mode_enables_tls},
 #endif
+    {"nested_values_rfc5424", flb_test_nested_keys_rfc5424},
     {NULL, NULL}
 };
